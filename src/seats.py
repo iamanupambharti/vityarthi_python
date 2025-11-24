34733738a16010672d05f35782e404e5122c46b4
Anupam Bharti <anupam2k7bharti@gmail.com>
2025-11-24 09:18:20 +0530

    Adding allocate , release in seats.py

--- src/seats.py
+++ src/seats.py
@@ -18,3 +18,34 @@ class SeatManager:
 
     def list_seats(self):
         return self.seats
+
+    def allocate(self, seat_id, user_id, user_mgr=None):
+
+        if seat_id not in self.seats:
+            return False
+
+        if self.seats[seat_id] is not None:
+            return False
+
+        if user_mgr:
+            if user_mgr.find_user(user_id) is None:
+                return False
+
+        self.seats[seat_id] = user_id
+        return True
+
+    def release(self, seat_id):
+        if seat_id not in self.seats:
+            return False
+
+        if self.seats[seat_id] is None:
+            return False
+
+        self.seats[seat_id] = None
+        return True
+
+    def to_dict(self):
+        return {
+            "total_seats": self.total_seats,
+            "seats": {str(k): self.seats[k] for k in self.seats}
+        }
