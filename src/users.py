81031acd28f4f194ddd7857f6d6e017de23b0b5f
Anupam Bharti <anupam2k7bharti@gmail.com>
2025-11-24 09:22:30 +0530

    Adding some missing lines in users.py

--- src/users.py
+++ src/users.py
@@ -1,26 +1,25 @@
-# src/users.py
-"""
-Stage 2: Added create_user() method.
-Using simple dicts for storing user data.
-"""
-
-import uuid  # using uuid for simple unique ids (beginner friendly)
-
+import uuid
 class UserManager:
     def __init__(self, initial_data=None):
-        # keep user data in a list of dicts
         if initial_data is None:
             initial_data = []
-
         self.users = initial_data
 
     def create_user(self, name):
-        """
-        Create a user with a short ID.
-        For now, just keep it simple: id + name.
-        """
-        # making the id short so it is easy to type
+        # short id using uuid
         user_id = str(uuid.uuid4())[:8]
-
         user = {
-            "id": user
+            "id": user_id,
+            "name": name
+        }
+        self.users.append(user)
+        return user_id
+
+    def list_users(self):
+         return self.users
+
+    def find_user(self, user_id):
+        for user in self.users:
+            if user["id"] == user_id:
+                return user
+        return None
